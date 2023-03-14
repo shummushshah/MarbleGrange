9dc91c0698914fde3cfe306404f03609d804d8e5
Shummus Shah <shummusshah@Shummuss-MacBook-Air.local>
2023-03-14 16:00:07 +0000

    New site

--- assets/js/main.js
+++ assets/js/main.js
@@ -273,6 +273,60 @@ $('.services-active').slick({
   });
 
 
+
+ // toggle 
+$(document).ready(function(){
+    
+    $('.search-toggle').click(function(){
+        $('.search-wrapper').toggleClass('show');
+    });
+
+    $('.modal-toggle').click(function(){
+        $('.modalBox').toggleClass('show');
+    })
+
+    $('.modalBox').click(function(){
+        $(this).removeClass('show');
+    });
+
+    $('.spinner').click(function(){
+        $(".theme-selector").toggleClass('show');
+    });
+    $('.light').click(function(){
+        $('body').addClass('light-theme');
+        $('body').removeClass('dark-theme');
+    });
+    $('.dark').click(function(){
+        $('body').toggleClass('dark-theme');
+        $('body').removeClass('light-theme');
+    });
+});
+
+
+
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+}); 
+
+
+
+
+
 //17.  Progress barfiller
 
   $('#bar1').barfiller();
@@ -283,3 +337,26 @@ $('.services-active').slick({
   $('#bar6').barfiller();
 
 })(jQuery);
+
+
+$(document).ready(function() {
+    $('#faqs h3').each(function() {
+        var tis = $(this), 
+            state = false, 
+            answer = tis.next('div')
+                        .hide()
+                        .css('height','auto')
+                        .slideUp();
+
+            tis.click(function() {
+                state = !state;
+                answer.slideToggle(state);
+                tis.toggleClass('active',state);
+            });
+    });
+});
+
+
+
+
+
